d59280e346fd20a765d55c4f5f96f900b4cf424d
Nour El Gafy <nour-elgafy@outlook.com>
2019-03-23 00:55:51 +0200

    initial code

---
+++ FaceRecog.py
@@ -0,0 +1,63 @@
+import face_recognition
+import cv2
+
+video_capture = cv2.VideoCapture(0)
+
+img = cv2.imread("Nour.JPG")
+resized_image = cv2.resize(img, (500, 500))
+resized_imageencoding = face_recognition.face_encodings(resized_image)[0]
+known_face_encodings=[]
+known_face_names=[]
+known_face_encodings.append(resized_imageencoding)
+known_face_names.append("Nour")
+
+while True:
+    # Grab a single frame of video
+    ret, frame = video_capture.read()
+
+    # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+    rgb_frame = frame[:, :, ::-1]
+
+    # Find all the faces and face enqcodings in the frame of video
+    face_locations = face_recognition.face_locations(rgb_frame)
+    face_encodings = face_recognition.face_encodings(rgb_frame, face_locations)
+
+    # Loop through each face in this frame of video
+    for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
+        # See if the face is a match for the known face(s)
+        matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+
+        name = "Unknown"
+
+        # If a match was found in known_face_encodings, just use the first one.
+        if True in matches:
+            first_match_index = matches.index(True)
+            name = known_face_names[first_match_index]
+
+        # Draw a box around the face
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+
+        # Draw a label with a name below the face
+        cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+        font = cv2.FONT_HERSHEY_DUPLEX
+        cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+
+    # Display the resulting image
+    cv2.imshow('Video', frame)
+
+    # Hit 'q' on the keyboard to quit!
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release handle to the webcam
+video_capture.release()
+cv2.destroyAllWindows()
+#unknown_picture = face_recognition.load_image_file("Nour2.jpg")
+#unknown_face_encoding = face_recognition.face_encodings(unknown_picture)[0]
+
+#results = face_recognition.compare_faces([my_face_encoding], unknown_  face_encoding)
+#print(results)
+
+#resized_image = cv2.resize(img, (750, 750))
+#face_locations = face_recognition.face_locations(resized_image)
+#print(len(face_locations))
